363cf2146c023ce32b7e382b3af168b56868701c
DharshiniL2005 <162532244+DharshiniL2005@users.noreply.github.com>
2026-01-02 08:38:43 +0530

    Add files via upload

---
+++ FollowUp.java
@@ -0,0 +1,50 @@
+import java.util.ArrayList;
+import java.util.List;
+
+class FollowUp {
+
+    public static List<String> expandString(String input) {
+        List<String> result = new ArrayList<>();
+
+        int open = input.indexOf('{');
+        int close = input.indexOf('}');
+
+    
+        if (open == -1 || close == -1 || close < open) {
+            result.add(input);
+            return result;
+        }
+
+        String middle = input.substring(open + 1, close);
+
+        if (middle.isEmpty()) {
+            result.add(input);
+            return result;
+        }
+
+        String[] tokens = middle.split(",");
+
+        if (tokens.length < 2) {
+            result.add(input);
+            return result;
+        }
+
+        String prefix = input.substring(0, open);
+        String suffix = input.substring(close + 1);
+
+        for (String token : tokens) {
+            result.add(prefix + token + suffix);
+        }
+
+        return result;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(expandString("sun{mars}rotation"));
+        System.out.println(expandString("minimum{}change"));
+        System.out.println(expandString("hello-world"));
+        System.out.println(expandString("hello-{-world"));
+        System.out.println(expandString("hello-}-weird-{-world"));
+        System.out.println(expandString("/2022/{jan,feb,march}/report"));
+    }
+}
